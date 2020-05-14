7993f11b15d12189ca1ee7326b6226b42ada4982
Ahmad-Sawalqeh <53290785+Ahmad-Sawalqeh@users.noreply.github.com>
2020-05-14 15:22:30 +0300

    life-cycle methods

---
+++ src/index.js
@@ -0,0 +1,15 @@
+    
+import React from 'react';
+import { render } from 'react-dom';
+import App from './app.js';
+
+
+const Main = () => {
+    return (
+        <>
+            <App />
+        </>
+    )
+}
+
+render(<Main />, document.getElementById('root'));
